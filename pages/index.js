cff40960269af611038dd4d1d5c6fc10f31f4d64
bmstyojaop <bmstyojaop@gmail.com>
2020-09-30 00:37:48 +0900

    スタイルシートの分割

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import style from '../static/Style';
+
+
+export default () =><div>
+  {style}
+  <h1>Next.js</h1>
+  <p>Welcome to next.js!</p>
+  <hr />
+  <div>
+    <Link href="/other">
+      <button>Go to other page &gt;&gt;</button>
+    </Link>
+  </div>
+</div>
